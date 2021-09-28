c88621851fc3dfbdbd9828459f4df558d070c824
killerpenguin12 <35267092+killerpenguin12@users.noreply.github.com>
2021-09-28 15:30:08 -0600

    Update DP1.cpp
    Fixed some bugs and found new ones. Also dont use vector methods

--- DP1.cpp
+++ DP1.cpp
@@ -33,34 +33,24 @@ while ((input_file >> a >> b >> c) && (b == ','))
     //plist->*next = plist[i+1]
 }
 
-/*vector<string> data;
-cout << "1st" << "\n";
-int i = 0;
-cout << "2nd\n";
-string line;
-        while(getline(input_file,line))
-        {
-            //cout << "Here?\n";
-            data.push_back(line);
-            //cout << data[i] << "\n";
-            cout << i/100 << "\n";
-            i++;
-        }
-        
-//input_file.close();
-    
-//if() cout << "Unable to open file";
-cout << (data[10]) << "\n";
-cout << (data[1]) << "\n";*/
 }
 
-void write(vector<Point> p)
+void write(vector<Point> p,int size)
 {
+
 ofstream output_file("p100_out.txt");
+
 if(!output_file.is_open())
     throw runtime_error("File not found.");
-for(int i = 0; i < p.size(); i++ )
+//cout << "Do we get into write?" << endl;
+//cout << "Here: " << p[1].x << endl;
+//cout << "did it like that?" << endl;
+for(int i = 0; i < 10; i++ )
+    {
+    //cout << p[i].x << endl;
     output_file << p[i].x << " , " << p[i].y ;
+    
+    }
 //writing back to our file the points we want
 }
 
@@ -91,71 +81,70 @@ int comparey(Point one, Point two)
     return 0;
 }
 
-void algorithm()
-{
-//Here our algorithm runs to calc what Points we need
-}
-
-int getLine(Point one,Point two,Point c)
+int cross(Point& one,Point& two, Point& c)
 {
 //Get the line that Points can be above or below
-// y = mx + b for original line.
-   // float m = two.y - one.y/(two.x - one.x); //getting our m
-    //get our b:
-   // b = one.y - m * one.x;
-   Point v1.x = 1;
+   float x1 = two.x - one.x;
+   float y1 = two.y - one.y;
 
-   Point v2 {0,0};
-   Point v1.x = two.x - one.x;
-   Point v1.y = two.y - one.y;
-
-   Point v2.x = two.x - c.x;
-   Point v2.y = two.y - c.y;
-   float xp = v1.x*v2.y - v1.y*v2.x;
+   float x2 = two.x - c.x;
+   float y2 = two.y - c.y;
+   float xp = x1*y2 - y1*x2;
    if(xp > 0) return 1; //below, or to the right of the line from one to two.
    else if(xp < 0) return -1; //above, or to the left of the line.
    else return 0; //on the same line
 
 return 0;
 }
-
-void removal(vector<Point> &plist)
+//Need to make a new resize function as this one does not work with point. SO thats a bummer.
+void resizeVec(vector<Point> &p,int newCap)
 {
- // Here is where we are supposed to remove all Points inbetween the original vertex and the Point 
- //with the same x and greatest y coordinate. Not sure how to do that yet.
+ //I dont want to do this. SO i wont for now.
 }
 
 int main()
 {
-    n = 100;
+    int n = 100;
     //b = bucketSize;
-    b = 10;
+    int b = 10;
     vector<Point>plist(n);
     read(plist);
     vector<Point>savedPoints(4); //contains all our best highest values of each bucket.
-    vector<Point>finalShapecw(2*b); //dont want to resize this. not sure how big we should make it.
-    vector<Point>finalShapeccw(2*b); // we will merge these at the end
-
-    /*for (int i = 0; i < plist.size(); i++)
-    std::cout << "x: " << plist[i].x << " y: " << plist[i].y << "           \n";
-    std::cout << "\n";*/
+    vector<Point>finalShapecw(b*4); //dont want to resize this. not sure how big we should make it.
+    vector<Point>finalShapeccw(b*4); // we will merge these at the end
     
     std::sort(plist.begin(),plist.end(),comparex);
     // next we seperate into buckets, which will change our resolution based on how big these buckets are
-    int i = 0;
+    
     finalShapecw[0].x = plist[0].x;
     finalShapecw[0].y = plist[0].y; // our highest x should be included in our shape
-    finalShapecww[0].x = plist[0].x;
-    finalShapecww[0].y = plist[0].y;
+    finalShapeccw[0].x = plist[0].x;
+    finalShapeccw[0].y = plist[0].y;
+    //Point last = plist.back(); // saving our smallest x
+    //cout << "Here it is: " << plist.back() << endl;
+    int sizecw = 1;
+    int capcw = b*4;
+    
+    int sizeccw = 1;
+    int capccw = b*4;
+
+    //cout << "Here is the size: " << sizeccw << " Here is the capacity: " << capccw << endl; 
+    //int i = 0;
     vector<Point>bucket(b);
-    for (i;i < n;i++)
+    for (int i;i < n;i++)
     {
-        if(i%b > 0)
-            bucket[i%b] = plist[i]; //filling up our bucket
+        if(i%b > 0){
+            bucket[(i%b)] = plist[i]; //filling up our bucket
+            cout << (i%b) << endl;
+        }
         else if(i %b == 0)
             {
             //bucket is full so we can compare
-            std::sort(bucket.begin(),bucket.end,comparey) //deciding what order the values go.
+            std::sort(bucket.begin(),bucket.end(),comparey); //deciding what order the values go.
+            //for(int i = 0; i < b; i++)
+            //{
+                //cout << "Bucket: " << bucket[i].x  << " , " << bucket[i].y << endl;
+            //}
             savedPoints[0].x = bucket[0].x; //smallest and largets Points from our bucket.
             savedPoints[0].y = bucket[0].y;
             savedPoints[1].x = bucket[1].x; //taking top and bottom two to make sure we are not missing any.
@@ -168,42 +157,90 @@ int main()
             //  We will use our getline function to see which ones will be best. 
             //going on top ccw and bottom cw. so if it is within on the top it is -1.
             //going on bottom cw so if it is within the line it is 1.
-            if(getline(finalShapeccw[i],savedPoints[0],savedPoints[1]) == -1) //topside going ccw
+            if(capccw == sizeccw)
+            {
+                //cout << "Let me know if this triggers 1" << endl;
+                resizeVec(finalShapeccw,finalShapeccw.capacity()*2); //this does not work. Throws an error. Maybe just use an array? 
+                capccw = capccw*2;
+            }
+            else if(cross(finalShapeccw[sizeccw],savedPoints[0],savedPoints[1]) == -1) //topside going ccw
                 { 
-                finalShapeccw[i+1].x = savedPoints[0].x; //this would make 0 the correct one
-                finalShapeccw[i+1].y = savedPoints[0].y;
+                finalShapeccw[sizeccw+1].x = savedPoints[0].x; //this would make 0 the correct one
+                finalShapeccw[sizeccw+1].y = savedPoints[0].y;
+                //cout << "does cross 1 trigger?" << endl;
+                
+                sizeccw += 1;
                 }
             else
                 {
-                finalShapeccw[i+1].x = savedPoints[1].x; //1 would be outside the line.
-                finalShapeccw[i+1].y = savedPoints[1].y;
+                finalShapeccw[sizeccw+1].x = savedPoints[1].x; //1 would be outside the line.
+                finalShapeccw[sizeccw+1].y = savedPoints[1].y;
+                //cout << "does cross 2 trigger?" << endl;
+                //cout << "x: " << savedPoints[0].x << " y: " << savedPoints[0].y << endl;
+                sizeccw += 1;
                 } 
-            if(getline(finalShapecw[i],savedPoints[3],savedPoints[2]) == 1) // then 2 would be inside
+            if(capcw == sizecw)
+            {
+                //cout << "Let me know if this triggers 2" << endl;
+                resizeVec(finalShapecw,finalShapecw.capacity()*2);
+                capcw = capcw*2;
+            }
+            else if(cross(finalShapecw[sizecw],savedPoints[3],savedPoints[2]) == 1) // then 2 would be inside
                 {
-                    finalShapecw[i+1].x = savedPoints[3].x; 
-                    finalShapecw[i+1].y = savedPoints[3].y;
+                    finalShapecw[sizecw+1].x = savedPoints[3].x; 
+                    finalShapecw[sizecw+1].y = savedPoints[3].y;
+                    //cout << "does cross 3 trigger?" << endl;
+                    sizecw += 1;
                 }
             else 
                 {
-                    finalShapecw[i+1].x = savedPoints[2].x; 
-                    finalShapecw[i+1].y = savedPoints[2].y;
+                    finalShapecw[sizecw+1].x = savedPoints[2].x; 
+                    finalShapecw[sizecw+1].y = savedPoints[2].y;
+                    //cout << "does cross 4 trigger?" << endl;
+                    sizecw += 1;
                 }
             } 
     }
 
     //Finally merge our two lists of important Points.
     //the two vectors have identical initial Points so we will delete one.
-    finalShapeccw.erase(finalShapeccw.begin()); //erasing duplicate
+    //finalShapeccw.erase(finalShapeccw.begin()); //erasing duplicate
     //have to check if it can fit
-    while(finalShapecw.size() + finalShapeccw.size() > finalShapecw.capacity()) 
-        finalShapecw.resize(finalShapecw.capacity()*2);
-    a.insert(finalShapecw.end(), finalShapeccw.begin(), finalShapeccw.end());
-    //finally write the results
-    write(finalShapecw);
+    //cout << "Here is the size CCW: " << sizeccw << " Here is the capacity CCW: " << capccw << endl; 
+    //cout << "Here is the size CW: " << sizecw << " Here is the capacity CW: " << capcw << endl;
+    for(int i = 1; i < sizecw-1; i++) //dodge the first value, its a duplicate.
+    {
+        if(sizeccw == capccw)
+        {
+           // cout << "Let me know if this triggers 3" << endl;
+            //finalShapeccw.resize(capccw*2); //double our capacity
+            capccw = capccw*2;
+        }
+        else
+        {
+            //std::cout << "x: " << finalShapeccw[i].x << " y: " << finalShapeccw[i].y << "           \n";
+            //cout << "Let me know if this triggers 4" << endl;
+            finalShapeccw[sizeccw + i - 1].x = finalShapecw[i].x; 
+            finalShapeccw[sizeccw + i - 1].y = finalShapecw[i].y;
+            //cout << "Let me know if this triggers 5" << endl;
+        }
+    }
+    //cout << "Do we make it here? " << endl;
 
-    //std::sort(plist.begin(),plist.end(),comparey);
-    //if(plist[0].x == plist[1].x) removal(plist);
-    //Point vertex = plist[0];
+    //finally write the results*/
+    finalShapeccw[sizeccw-1].x  = plist[n].x;
+    finalShapeccw[sizeccw-1].y  = plist[n].y;
+    sizeccw += 1;
+    //for (int i = 0; i < sizecw; i++)
+    //    std::cout << "x: " << finalShapecw[i].x << " y: " << finalShapecw[i].y << "           \n";
+
+    //cout << "What about here?" << endl;
+
+
+    //finalShapeccw.push_back(last);
+    write(finalShapeccw,sizeccw);
+
+    
 
     /*cout << " Sorted list: \n";
     for (int i = 0; i < plist.size(); i++)
